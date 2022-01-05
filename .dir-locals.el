af7eb6a54d0aaacdef1fde63f777830932d5671f
David Wilson <david@daviwil.com>
2022-01-05 06:22:09 +0200

    Set `compile-command` in `.dir-locals.el`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((geiser-scheme-implementation . guile)
+         (compile-command . "guix shell --pure -m manifest.scm -- make -C build")
          (eval . (progn (add-to-list 'load-path (expand-file-name "tools"))
                         (require 'flux-compose))))))
